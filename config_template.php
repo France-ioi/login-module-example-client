124b7fb2dbf062d4037bfbccea441cb54c1af63a
mobydimk <mobydimk@gmail.com>
2017-02-13 06:15:15 +0400

    config tpl

--- config_template.php
+++ config_template.php
@@ -1,15 +1,24 @@
 <?php
 
+$client_host = 'http://login-module-example-client.dev';
 $login_module_host =  'http://login-module.dev';
 
 $config = [
     'oauth' => [
         'clientId' => '3',
         'clientSecret' => 'l5B9AWOtpCr4sVHkctF1erU2eiPypYG7K4fi9mEq',
-        'redirectUri' => 'http://login-module-example-client.dev/oauth_callback.php',
         'urlAuthorize' => $login_module_host.'/oauth/authorize',
         'urlAccessToken' => $login_module_host.'/oauth/token',
-        'urlResourceOwnerDetails' => $login_module_host.'/api/user_profile'
-    ]
+        'urlResourceOwnerDetails' => $login_module_host.'/api/account',
+        'redirectUri' => $client_host.'/callback_oauth.php'
+    ],
+    'logout' => [
+        'url' => $login_module_host.'/logout',
+        'redirectUri' => $client_host.'/callback_logout.php'
+    ],
+    'account' => [
+        'url' => $login_module_host.'/account',
+        'redirectUri' => $client_host.'/callback_account.php'
+    ],
 
 ];
