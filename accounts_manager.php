3f8552e51fb0b19a75f97f39115534c3a4c36a8d
mobydimk <mobydimk@gmail.com>
2017-12-20 22:46:20 +0400

    autologin

--- accounts_manager.php
+++ accounts_manager.php
@@ -2,16 +2,14 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/config.php';
-
 function run($action) {
     global $config;
     $client = new FranceIOI\LoginModuleClient\Client($config['login_module']);
     $manager = $client->getAccountsManager();
-
     try {
         switch($action) {
             case 'create':
-                $res = $manager->create($_POST['prefix'], $_POST['amount']);
+                $res = $manager->create($_POST['prefix'], $_POST['amount'], isset($_POST['auto_login']));
                 break;
             case 'delete':
                 $res = $manager->delete($_POST['prefix']);
@@ -25,6 +23,10 @@ function run($action) {
 
     echo '<pre>'.var_export($res, true).'</pre>';
 }
+
+$prefix = isset($_POST['prefix']) ? $_POST['prefix'] : 'test';
+$amount = isset($_POST['amount']) ? $_POST['amount'] : '1';
+$auto_login = isset($_POST['auto_login']);
 ?>
 <!DOCTYPE html>
 <html>
@@ -39,10 +41,16 @@ function run($action) {
         <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
             <input type="hidden" name="action" value="create"/>
             <div>
-                Prefix <input type="text" name="prefix" value="<?=isset($_POST['prefix'])?$_POST['prefix']:'test'?>"/>
+                Prefix <input type="text" name="prefix" value="<?=$prefix?>"/>
             </div>
             <div>
-                Amount <input type="text" name="amount" value="<?=isset($_POST['amount'])?$_POST['amount']:'1'?>"/>
+                Amount <input type="text" name="amount" value="<?=$amount?>"/>
+            </div>
+            <div>
+                <label>
+                    <input type="checkbox" name="auto_login" <?=($auto_login ? 'checked="checked"' : '')?>/>
+                    Create auto login token
+                </label>
             </div>
             <input type="submit"/>
         </form>
@@ -51,7 +59,7 @@ function run($action) {
         <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
             <input type="hidden" name="action" value="delete"/>
             <div>
-                Prefix <input type="text" name="prefix" value="<?=isset($_POST['prefix'])?$_POST['prefix']:'test'?>"/>
+                Prefix <input type="text" name="prefix" value="<?=$prefix?>"/>
             </div>
             <input type="submit"/>
         </form>
